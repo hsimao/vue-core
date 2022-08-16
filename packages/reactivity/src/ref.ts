22971b21d0df3718bce4a4a657137105c3d0768d
hsimao <e087754958@gmail.com>
2022-08-16 19:58:06 +0800

    feat: ref

---
+++ packages/reactivity/src/ref.ts
@@ -0,0 +1,34 @@
+import { isObject } from '@vue/shared'
+import { reactive } from './reactive'
+import { trackEffects, triggerEffects } from './effect'
+
+function toReactive(value) {
+  return isObject(value) ? reactive(value) : value
+}
+
+class RefImpl {
+  public _value
+  public dep = new Set()
+  public __v_isRef = true
+
+  constructor(public rawValue) {
+    this._value = toReactive(rawValue)
+  }
+
+  get value() {
+    trackEffects(this.dep)
+    return this._value
+  }
+
+  set value(newValue) {
+    if (newValue !== this.rawValue) {
+      this._value = toReactive(newValue)
+      this.rawValue = newValue
+      triggerEffects(this.dep)
+    }
+  }
+}
+
+export function ref(value) {
+  return new RefImpl(value)
+}
