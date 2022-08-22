cfed4d6c955e91e50d1f4594bab862d2afa9fa16
hsimao <e087754958@gmail.com>
2022-08-22 14:17:36 +0800

    feat: runtime core basic setting

---
+++ packages/runtime-core/src/renderer.ts
@@ -0,0 +1,8 @@
+export function createRenderer(renderOptions) {
+  // 渲染過程用傳入的 renderOptions 來處理
+  const render = (vnode, container) => {}
+
+  return {
+    render
+  }
+}
--- packages/runtime-dom/src/index.ts
+++ packages/runtime-dom/src/index.ts
@@ -1,6 +1,11 @@
+import { createRenderer } from '@vue/runtime-core'
 import { nodeOps } from './nodeOps'
 import { patchProp } from './patchProp'
 
+// 瀏覽器的渲染 API domAPI
 const renderOptions = Object.assign(nodeOps, { patchProp })
 
-console.log('renderOptions', renderOptions)
+export function render(vnode, container) {
+  // 創建渲染器, 傳入 renderOptions
+  createRenderer(renderOptions).render(vnode, container)
+}
